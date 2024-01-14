41d6e52560c40bb7c2dce0f770bf20d9bd1fdb82
Marc Roma <marc.roma@cirvianum.cat>
2024-01-14 22:37:30 +0100

    Sync - Designs for mobile 430px wide | Done

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
       fontFamily: {
         glory: ['"Give You Glory" ', "cursive"],
         inter: ["Inter", "sans-serif"],
+        jetbrains: ["JetBrains Mono", "monospace"]
       },
     },
   },
